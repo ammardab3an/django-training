753151b64344474a57ac3ebb3d8ca2dbbbd259f6
AmmarDab3an <ammar.dabaan99@gmail.com>
2022-10-31 05:50:02 +0200

    convert registered email to lowercase

--- musicplatform/users/models.py
+++ musicplatform/users/models.py
@@ -3,4 +3,8 @@ from django.contrib.auth.models import AbstractUser
 # Create your models here.
 
 class ExUser(AbstractUser):
-    bio = models.CharField(max_length=256)
+    
+    bio = models.CharField(max_length=256, default="my simple bio.")
+    
+    def clean_email(self):
+        self.email = self.email.lower()
